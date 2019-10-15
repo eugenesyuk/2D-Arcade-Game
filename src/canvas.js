5a27f70e96196b7142d4505cc95007e1eae1320f
Eugene Syrotiuk <eugenesyuk@gmail.com>
2019-10-15 21:44:56 +0200

    Added sprites

--- src/canvas.js
+++ src/canvas.js
@@ -1,17 +1,17 @@
-import { ImageLoader } from "./image_loader";
+import { ImageLoader } from "./image-loader";
 
 export class Canvas {
   constructor(width, height) {
     this.width = width;
     this.height = height;
-    this.element = this.initCanvas();
-    this.setDimentions(this.width, this.height);
+    this.element = this.init();
+    this.setSize(this.width, this.height);
     this.context = this.getContext();
     this.images = {};
     this.imagesLoaded = false;
   }
 
-  initCanvas() {
+  init() {
     let canvas = document.body.querySelector('canvas');
   
     if(!canvas) {
@@ -22,7 +22,7 @@ export class Canvas {
     return canvas;
   }
 
-  setDimentions(width, height) {
+  setSize(width, height) {
     this.element.width = width;
     this.element.height = height;
   }
@@ -36,7 +36,7 @@ export class Canvas {
     loader.load().then(names => {
       this.images = Object.assign(this.images, loader.images);
       this.imagesLoaded = true;
-    })
+    });
   }
 
   fill(color) {
@@ -44,7 +44,7 @@ export class Canvas {
     this.context.fillRect(0, 0, this.width, this.height);
   }
 
-  print(str, x, y) {
+  drawText(str, x, y) {
     this.context.fillStyle = '#ffffff';
     this.context.font = '32px Arial';
     this.context.fillText(str, x, y); 
@@ -52,5 +52,11 @@ export class Canvas {
 
   drawImage(image, x, y) {
     this.context.drawImage(this.images[image], x, y);
+  }
+
+  drawSprite(sprite) {
+    this.context.drawImage(this.images[sprite.imageName], 
+      sprite.sourceX, sprite.sourceY, sprite.width, sprite.height,
+      sprite.x, sprite.y, sprite.width, sprite.height);
   }
 }
--- src/scenes/level_1.js
+++ src/scenes/level_1.js
@@ -1,12 +1,27 @@
 import { Scene } from "../scene";
+import { SpriteSheet } from "../sprite-sheet";
 
 export class Level_1 extends Scene {
   constructor(game) {
     super(game);
+
+    this.tiles = new SpriteSheet({
+      imageName: 'tiles',
+      imageWidth: 640,
+      imageHeight: 640
+    })
+
+    this.tree = this.tiles.getSprite(7);
+    this.tree.setCoordinates(10, 10);
+
+    this.sand = this.tiles.getSprite(22);
+    this.sand.setCoordinates(300, 400);
   }
 
   render(time) {
     this.game.canvas.fill('#efefef');
+    this.game.canvas.drawSprite(this.sand);
+    this.game.canvas.drawSprite(this.tree);
     super.render(time);
   }
 }
